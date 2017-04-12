ca617c0699512e57b212b35cdb6941bab3ce6804
camille <camille.meyrignac@etu.u-bordeaux.fr>
2017-04-12 16:25:33 +0200

    connection client-server + création client/serveur

--- reseau.py
+++ reseau.py
@@ -2,3 +2,21 @@ from game import *
 
 import socket
 import select
+
+
+
+def createServer():
+    s = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,0)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind(('',7777))
+    s.listen(1)
+    return s
+
+
+def createClient(IP,port):
+    s = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,0)
+    s.connect((IP,int(float(port))))
+    return s
+
+
+ 
