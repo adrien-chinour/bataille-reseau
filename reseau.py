77e8e52cda316c8ce8ce1462d6016e59c9047af8
camille <camille.meyrignac@etu.u-bordeaux.fr>
2017-04-27 15:40:07 +0200

    ajout début TLS

--- reseau.py
+++ reseau.py
@@ -2,7 +2,8 @@ from game import *
 
 import socket
 import select
-
+import subprocess
+import sys,os
 
 """ Demarrage du serveur et ecoute sur le port 7777 """
 def createServer():
@@ -27,3 +28,38 @@ def sendMessage(sender,recipient,message,IP,port):
         l[0].close()
         client = createClient(sys.argv[1],sys.argv[2])
         l = [client]
+
+def createAutorite():
+    if(not os.path.isfile('ca.key')):
+        createKey = ['certtool','--generate-privkey','--outfile', 'ca.key']
+        p = subprocess.Popen(createKey, stdout=subprocess.PIPE)
+    if(not os.path.isfile('ca.crt')):
+        createCertif = ['certtool', '--generate-self-signed', '--template', 'cert.cfg', '--load-privkey', 'ca.key', '--outfile', 'ca.crt']
+        p = subprocess.Popen(createCertif, stdout=subprocess.PIPE)
+    
+def createKeyServer():
+    tree = b''
+    #cmdexist = ['if', '[','-f','"server.key"', ']',';','then','echo','"ok"',';','fi']
+    #p = subprocess.Popen(cmdexist, stdout=subprocess.PIPE)
+    if(not os.path.isfile('server.key')):
+        cmd = ['certtool','--generate-privkey','--outfile', 'server.key']
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    #for line in p.stdout:
+    #    tree+=line
+    #print(tree.decode())
+    #p.wait()
+    #print(p.returncode)
+
+def verifCertif(socket):# le client récupère un certificat
+    if(not os.path.isfile('ca.crt')):
+        socket.send('CRT')
+        f = open('ca.crt','wb')
+        l = socket.recv(1024)
+        while(l)
+            f.write(l)
+            l = socket.recv(1024)
+        f.close()
+    print('ok certif\n')
+
+
+    
