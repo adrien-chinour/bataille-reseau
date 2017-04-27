ef99207e6e997791a6d03c4fe1429e9351f0e3a9
Adrien Chinour <adrienchinour@gmail.com>
2017-04-27 15:46:14 +0200

    ajout rapport

--- reseau.py
+++ reseau.py
@@ -7,7 +7,7 @@ import select
 """ Demarrage du serveur et ecoute sur le port 7777 """
 def createServer():
     s = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,0)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) #au cas ou des connection serait encore en cours sur le port
     s.bind(('',7777))
     s.listen(1)
     return s
